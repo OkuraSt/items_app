98e4ed734317c49fe85d0ec49168a3d1fb87c432
Vedha Ivan Barrientos Pantoja <vedha.ivan@gmail.com>
2017-03-07 00:00:13 -0600

    Configuracion modo ssr

--- src/apollo_client.js
+++ src/apollo_client.js
@@ -4,6 +4,7 @@
 
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 const client = new ApolloClient({
+  ssrMode: true,
   networkInterface: createNetworkInterface({ uri: 'http://localhost:8080/graphql' }),
 });
 export default client;
